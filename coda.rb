df0157af4cc2eb727b80ebd0c36a1440e744492b
Avery Morin <avery.schmavery@gmail.com>
2019-07-19 14:28:32 -0700

    Update coda.rb

--- coda.rb
+++ coda.rb
@@ -2,7 +2,7 @@ class Coda < Formula
   desc "This is a test"
   homepage "https://github.com/CodaProtocol/coda"
   url "https://s3-us-west-2.amazonaws.com/wallet.o1test.net/coda-test.tar"
-  sha256 "be048be525131c1ec6b635e79b691d61ae5f2bbd85872faf1293cc8ffb130925"
+  sha256 "a93bb08ef66509db50f2683d77a33fccbf26dcf6175d4bb58a8f1333194a6625"
 
   depends_on "openssl"
   depends_on "libsodium"
