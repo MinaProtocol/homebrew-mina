d14baa31dd4bc860e157c8416403e0c7ac54d1a3
Avery Morin <avery.schmavery@gmail.com>
2019-07-22 15:05:25 -0700

    Add a revision to get it to auto-update

--- coda.rb
+++ coda.rb
@@ -3,6 +3,7 @@ class Coda < Formula
   homepage "https://github.com/CodaProtocol/coda"
   url "https://s3-us-west-2.amazonaws.com/wallet.o1test.net/coda-test.tar.gz"
   sha256 "0c0892fd70b74c225a50d7adba672ac769b9964f7ce8cc78042a80fb4a522bd7"
+  revision 1
 
   depends_on "openssl"
   depends_on "libsodium"
