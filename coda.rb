e40c8b0d766db48815162d5962b36a943abc85ea
Brandon Kase <bkase@o1labs.org>
2019-10-08 12:29:12 -0700

    0.0.9-beta2

--- coda.rb
+++ coda.rb
@@ -1,9 +1,9 @@
 class Coda < Formula
   desc "Coda is the first cryptocurrency protocol with a succinct blockchain."
   homepage "https://github.com/CodaProtocol/coda"
-  url "https://github.com/CodaProtocol/coda/releases/download/0.0.8-beta/homebrew-coda.tar.gz"
-  sha256 "faa45f055fd640e6d4643fcd62af52c3ece36a72d75162315a77ff4d03298a1e"
-  revision 13
+  url "https://github.com/CodaProtocol/coda/releases/download/0.0.9-beta2/homebrew-coda.tar.gz"
+  sha256 "db98e9f618b9509e0126b5a0d6632ff05f46fdedf1066ea0469e98c63db16809"
+  revision 14
 
   depends_on "openssl"
   depends_on "libsodium"
