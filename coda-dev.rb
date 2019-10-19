d20e01b17b383ddae5eec1e293326e40283ae1eb
Nathan Holland <nholland94@gmail.com>
2019-10-18 17:24:30 -0700

    Add coda-dev package

--- coda-dev.rb
+++ coda-dev.rb
@@ -1,18 +1,19 @@
-class Coda < Formula
+class CodaDev < Formula
   desc "Coda is the first cryptocurrency protocol with a succinct blockchain."
   homepage "https://github.com/CodaProtocol/coda"
-  url "https://github.com/CodaProtocol/coda/releases/download/0.0.9-beta4/homebrew-coda.tar.gz"
-  sha256 "5aee00bec49b0f823964b4a14abbd722ac53335a346ccbc74fafe30456f02d2b"
-  revision 15
+  url "https://github.com/CodaProtocol/coda/releases/download/0.0.10-beta/homebrew-coda.tar.gz"
+  sha256 "ab57a8f7e1a29508e376b0bf868021470b8f7489899a75b5f58565f6edfcfd00"
+  revision 1
 
-  depends_on "openssl"
-  depends_on "libsodium"
   depends_on "gmp"
+  depends_on "jemalloc"
   depends_on "libomp"
+  depends_on "libsodium"
+  depends_on "openssl"
   depends_on "libffi"
   depends_on "miniupnpc" => :recommended
 
-  conflicts_with "coda-dev"
+  conflicts_with "coda"
 
   bottle :unneeded
 
@@ -20,6 +21,7 @@ class Coda < Formula
     bin.install("coda")
     bin.install("kademlia")
     bin.install("coda-logproc")
+    bin.install("coda-libp2p_helper")
     (var/"coda").install Dir["keys/*"]
   end
 end
