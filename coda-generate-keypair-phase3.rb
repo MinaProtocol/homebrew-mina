4576a3ac64735c2a2f47fefd8eedc9d94c42bcd6
Joel Krauska <jkrauska@gmail.com>
2020-01-13 18:34:24 -0800

    new file

--- coda-generate-keypair-phase3.rb
+++ coda-generate-keypair-phase3.rb
@@ -2,9 +2,8 @@ class CodaGenerateKeypairPhase3 < Formula
   desc "Coda is the first cryptocurrency protocol with a succinct blockchain."
   homepage "https://github.com/CodaProtocol/coda"
   url "https://storage.googleapis.com/network-debug/coda-generate-keypair-phase3/homebrew-coda-generate-keypair-phase3.tar.gz"
-  sha256 "c724260d9863aa46fd7d4c63f5400895b636dd312f02608fb5f9a71846697eea"
-  revision 100
-
+  sha256 "3a19f5168f58501ec6de4c8f72fe49bd6f651537e37f4a8760e9d4bc8676d87a"
+  revision 101
   depends_on "openssl"
   depends_on "libsodium"
   depends_on "gmp"
