532c65594b0fad4d03518df2b45222d715efbb17
Carey Janecka <careyjanecka@gmail.com>
2020-09-18 19:37:40 -0700

    Install genesis proofs

--- coda.rb
+++ coda.rb
@@ -56,6 +56,6 @@ class Coda < Formula
     mkdir_p("/tmp/s3_cache_dir")
     bin.install("coda")
     bin.install("coda-logproc")
-    (var/"coda").install Dir["var/*"]
+    "/var/lib/coda/".install Dir["var/*"]
   end
 end
