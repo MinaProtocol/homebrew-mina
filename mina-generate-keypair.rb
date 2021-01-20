1005e92e4c2c10a61d0e563942d4be9961c012e0
Carey Janecka <careyjanecka@gmail.com>
2021-01-20 10:30:31 -0800

    Update mina-generate-keypair

--- mina-generate-keypair.rb
+++ mina-generate-keypair.rb
@@ -1,10 +1,10 @@
 class MinaGenerateKeypair < Formula
   desc "Mina is the first cryptocurrency protocol with a succinct blockchain."
   homepage "https://github.com/MinaProtocol/mina"
-  url "https://storage.googleapis.com/network-debug/mina-generate-keypair/homebrew-mina-generate-keypair.tar.gz"
+  url "https://s3-us-west-2.amazonaws.com/packages.o1test.net/0.0.17-beta9/homebrew-mina-generate-keypair.tar.gz"
   version "master"
-  sha256 "36705f6a296f3263defe1778df16502da9823329c2ba508ec73ade55e252e7d6"
-  revision 103
+  sha256 "53b61be71d1e38f32b9853cbe9fd50dc1f82945c3cfa4a67154fab8494b2f485"
+  revision 104
   depends_on "openssl"
   depends_on "libsodium"
   depends_on "gmp"
